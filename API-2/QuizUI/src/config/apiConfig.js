15339a3d1969e7523cd9ea9710cbe4c417db3381
VishalNyk <123799361+VishalNyk@users.noreply.github.com>
2025-10-22 00:02:40 +0530

    Update API_BASE_URL to production endpoint

--- API-2/QuizUI/src/config/apiConfig.js
+++ API-2/QuizUI/src/config/apiConfig.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:3000/api';
+//export const API_BASE_URL = 'http://localhost:3000/api';
+
+export const API_BASE_URL ='https://quiz-api-gg67.onrender.com/api';
