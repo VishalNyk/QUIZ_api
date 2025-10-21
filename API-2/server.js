2347a46942afd4d9226627546205350a72e5de32
VishalNyk <123799361+VishalNyk@users.noreply.github.com>
2025-10-22 00:13:43 +0530

    Restrict allowed HTTP methods for CORS

--- API-2/server.js
+++ API-2/server.js
@@ -21,8 +21,9 @@ app.use(cors({
         "http://localhost:5173",
         "http://127.0.0.1:5173" 
     ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    methods: "GET,HEAD,POST",
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'], // adding headers for preflight responses
 }));
 
 
